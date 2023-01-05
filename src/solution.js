764569a14d8f6fd1ddc3bbcb138b1e049c8650bc
Luis E. Sanchez Jr <113560203+lsan31296@users.noreply.github.com>
2023-01-05 15:34:33 -0500

    First commit. This code serves as a solution to a small Qualified Assessment on the ElephantSQL Instance Database.

---
+++ src/solution.js
@@ -0,0 +1,7 @@
+//Fill in the values from what is show in the Details section of your ElephantSQL Instance Database
+module.exports = {
+  host: "salt.db.elephantsql.com",//Server value
+  user: "mtqolxuw",//user and defaul database value
+  database: "mtqolxuw",//^
+  password: "MvwC2K9qqKjJR6flHTRWc9rSOXkbimrf",//Password value
+};
